8e2290789b3b60fe2664fea5c146b6359ac23595
valentina buoro <valentinabuoro4@gmail.com>
2023-11-14 21:42:50 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -7,9 +7,21 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+import { Ai } from '@cloudflare/ai';
+import { html } from './template.js';
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+	async fetch(request, env) {
+		// Create an instance of the AI using the provided environment variable.
+		const ai = new Ai(env.AI);
+
+		// Execute an inference task using Llama 2 for a given prompt.
+		const response = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
+			prompt: 'Who was the first president of Nigeria?',
+		});
+		
+
+		// Present the response as a JSON string.
+		return new Response(JSON.stringify(response));
 	},
 };
